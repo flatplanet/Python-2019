fa0ad0d091c638d3964bca5ba2a5c06ce9b29897
John Elder <johne4196@gmail.com>
2019-02-14 13:22:07 -0800

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+# Clear the terminal screen
+import os
+os.system('clear')
+
+#############################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+############################
+
+
+
+first_name = 'John'
+age = 41 
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"	
+	}
+name = True
+
+print (name)
+
+
